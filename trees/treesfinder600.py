9e8bb1a1b5d89e9eb012b00ad00745f8f051ce4c
B Cornell <b@Bs-MacBook-Pro.local>
2018-05-23 11:55:53 -0700

    Update treesfinders

--- trees/treesfinder600.py
+++ trees/treesfinder600.py
@@ -18,7 +18,7 @@ minx = minx * 82248
 maxx = maxx * 82248
 
 
-f = open("treefile_"+str(number1)+".csv", "w")
+#f = open("treefile_"+str(number1)+".csv", "w")
 later = open("late_treefile_"+str(number1)+".csv", "w")
 
 def sepvel(hax, hay, haz, hbx, hby, hbz, avx, avy, avz, bvx, bvy, bvz):
@@ -34,24 +34,24 @@ def sepvel(hax, hay, haz, hbx, hby, hbz, avx, avy, avz, bvx, bvy, bvz):
 		Vel_par = ((vx)*(x)+(vy)*(y)+(vz)*(z))/position
 		Vel_perp = math.sqrt((velocity*velocity)-(Vel_par*Vel_par))
 		return position, Vel_par, Vel_perp
-	else: 
+	else:
 		return 100, 100000, 100000
-		 
-	
-    
-    
-    
+
+
+
+
+
 for i in range(int(minx),int(maxx)): #cyclethrough every pair in the final data set
-	
-	postfinder = [[0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0]] 
-	
-	j = -1 
+
+	postfinder = [[0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0]]
+
+	j = -1
 	written = False
 	p = 0
 	haloa = rockstarida[i]
 	halob = rockstaridb[i]
-	
-    
+
+
 	for k in range(len(trowid)):
 		if (tdepthfirstid[k] < mainleafa[i] and tdepthfirstid[k] > depthfirsta[i]):
 			postfinder[int(tsnapnum[k]-69)][6] = tvx[k]
@@ -67,7 +67,7 @@ for i in range(int(minx),int(maxx)): #cyclethrough every pair in the final data 
 			postfinder[int(tsnapnum[k]-69)][3] = tx[k]
 			postfinder[int(tsnapnum[k]-69)][4] = ty[k]
 			postfinder[int(tsnapnum[k]-69)][5] = tz[k]
-			
+
 	smallest = 100
 	onvelarr = [0]*10
 	offvelarr = [0]*10
@@ -75,7 +75,7 @@ for i in range(int(minx),int(maxx)): #cyclethrough every pair in the final data 
 	while(p < len(postfinder)):
 		pairsep, paironvel, pairoffvel = sepvel(*postfinder[p])
 		separr[p] = pairsep
-		onvelarr[p] = paironvel 
+		onvelarr[p] = paironvel
 		offvelarr[p] = pairoffvel
 		if pairsep < 5:
 			print str(p+69) + ', ' + str(pairsep)
@@ -84,19 +84,16 @@ for i in range(int(minx),int(maxx)): #cyclethrough every pair in the final data 
 			passsnap = p
 			onvelsnap = paironvel
 			offvelsnap = pairoffvel
-			
+
 		p += 1
 	if smallest < .7:
-	   
+
 		string = str(pairid[i]) + ', ' + str(rockstarida[i]) + ', ' + str(rockstaridb[i]) + ', ' + str(massa[i]) + ', ' + str(massb[i]) + ', ' + str(radiusa[i]) + ', ' + str(radiusb[i]) + ', ' + str(separation[i]) + ', ' + str(vel_on[i]) + ', ' + str(vel_off[i]) + ', ' + str(passsnap+69) + ', ' + str(smallest) + ', ' + str(onvelsnap) + ', ' + str(offvelsnap)
-		f.write(string)            
-		f.write('\n')
+		#f.write(string)
+		#f.write('\n')
 		print string
 		latedata = str(pairid[i]) + ', ' + str(rockstarida[i]) + ', ' + str(rockstaridb[i]) + ', ' + str(massa[i]) + ', ' + str(massb[i]) + ', ' + str(radiusa[i]) + ', ' + str(radiusb[i]) + ', ' + str(separr[0]) + ', ' + str(onvelarr[0]) + ', ' + str(offvelarr[0]) + ', ' + str(separr[1]) + ', ' + str(onvelarr[1]) + ', ' + str(offvelarr[1]) + ', ' + str(separr[2]) + ', ' + str(onvelarr[2]) + ', ' + str(offvelarr[2]) + ', ' + str(separr[3]) + ', ' + str(onvelarr[3]) + ', ' + str(offvelarr[3]) + ', ' + str(separr[4]) + ', ' + str(onvelarr[4]) + ', ' + str(offvelarr[4]) + ', ' + str(separr[5]) + ', ' + str(onvelarr[5]) + ', ' + str(offvelarr[5]) + ', ' + str(separr[6]) + ', ' + str(onvelarr[6]) + ', ' + str(offvelarr[6]) + ', ' + str(separr[7]) + ', ' + str(onvelarr[7]) + ', ' + str(offvelarr[7]) + ', ' + str(separr[8]) + ', ' + str(onvelarr[8]) + ', ' + str(offvelarr[8]) + ', ' + str(separr[9]) + ', ' + str(onvelarr[9]) + ', ' + str(offvelarr[9]) + ', ' + str(separation[i])  + ', ' + str(vel_on[i]) + ', ' + str(vel_off[i])
-		
+
 		later.write(latedata)
 		later.write('\n')
 		print latedata
-            
-            
-
